681d658f2031e9350faf28da12d3299dc6b3e0af
juliafclima <juliafclima@hotmail.com>
2023-07-07 18:21:28 -0300

    operadores comparadores

---
+++ ex06/operadoresComparacao.js
@@ -0,0 +1,27 @@
+// IGUAL A ==
+console.log(10 == 10); //true
+console.log(12 == 10); //false
+
+// DIFERENTE A !=
+console.log(12 != 10); // true
+console.log(15 != 15); // false
+
+// MAIOR QUE >
+console.log(20 > 10); // true
+console.log(5 > 10); // false
+
+// MENOR QUE <
+console.log(10 < 20); //true
+console.log(20 < 10); // false
+
+// MAIOR OU IGUAL A >=
+console.log(20 >= 20); // true
+console.log(50 >= 50); // true
+
+// MENOR OU IGUAL A  <=
+console.log(50 <= 1); // false
+console.log(1 <= 1); // true
+
+// VALOR E TIPO IGUAIS A ===
+console.log(10 === 10); // true
+console.log(10 === "10"); // false
