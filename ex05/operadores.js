f19b87a6fbbbe9bda8f9edd230c3be41441be0ef
juliafclima <juliafclima@hotmail.com>
2023-07-07 17:49:17 -0300

    Update operadores.js

--- ex05/operadores.js
+++ ex05/operadores.js
@@ -1,33 +1,39 @@
 // OPERADORES ARITMETICOS 
 
-let num1 = 10;
-let num2 = 20;
+let num1 = 20;
+let num2 = 10;
 
 // ADIÇÃO +
 
 let totalAdicao = num1 + num2;
 
-console.log(`${num1} + ${num2} = ${totalAdicao}`);
+console.log(`SOMA: ${num1} + ${num2} = ${totalAdicao}`);
 
 // SUBTRAÇÃON -
 
 let totalSubtracao = num1 - num2;
 
-console.log(`${num1} - ${num2} = ${totalSubtracao}`);
+console.log(`SUBTRAÇÃO: ${num1} - ${num2} = ${totalSubtracao}`);
 
 // MULTIPLICACAO *
 
 let totalMultiplicacao = num1 * num2;
 
-console.log(`${num1} * ${num2} = ${totalMultiplicacao}`);
+console.log(`MULTIPLICAÇÃO: ${num1} * ${num2} = ${totalMultiplicacao}`);
 
 // DIVISÃO /
 
-let totalDivisao = num2 / num1;
+let totalDivisao = num1 / num2;
 
-console.log(`${num2} / ${num1} = ${totalDivisao}`);
+console.log(`DIVISÃO: ${num1} / ${num2} = ${totalDivisao}`);
 
 // RESTO DA DIVISÃO % (MOD)
 
-let restoDivisao = num1 % num2;
-console.log(restoDivisao);
+let totalResto = num1 % num2;
+console.log(`RESTO: ${num1} / ${num2} = ${totalResto}`);
+
+// INCREMENTO ++
+console.log(`INCREMENTO: ${num1} -> ${++num1}`);
+
+// DECREMENTO --
+console.log(`DECREMENTO: ${num2} -> ${--num2}`);
