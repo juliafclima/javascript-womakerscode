69eb73890a65d99a4cd7e5493a030a1e0163f9cf
juliafclima <juliafclima@hotmail.com>
2023-07-09 15:30:47 -0300

    Update array.js

---
+++ ex12/array.js
@@ -0,0 +1,49 @@
+// POP() - REMOVE O ÚLTIMO ELEMENTO
+
+let eletrodomesticos = [' 1 Televisão', ' 2 Geladeira', ' 3 Fogão'];
+
+console.log(`MÉTODO .POP()\nElementos: ${eletrodomesticos}`);
+console.log(`Último elemento: ${eletrodomesticos.pop()}`);
+console.log(`Com o .pop: ${eletrodomesticos}\n`);
+
+// PUSH() - ADICIONA UM NOVO ELEMENTO NO FINAL DO ARRAY
+
+let frutas = [' 1 Maçã', ' 2 Pera', ' 3 Laranja'];
+
+console.log(`MÉTODO .PUSH()\nElementos: ${frutas}`);
+console.log(`Qnt de elementos: ${frutas.push(' 4 Melancia')}`);
+console.log(`Com o .push: ${frutas}\n`); 
+
+// SHIFT() - REMOVE O PRIMEIRO ELEMENTO
+
+let animais = [' 1 Girafa', ' 2 Elefante', ' 3 Leão'];
+
+console.log(`MÉTODO .SHIFT()\nElementos: ${animais}`);
+console.log(`Primeiro elemento: ${animais.shift()}`);
+console.log(`Com o .shift: ${animais}\n`);
+
+
+// UNSHIFT() - ADICIONA UM NOVO ELEMENTO NO INICIO DO ARRAY
+
+let numeros = [' 2 Segundo', ' 3 Terceiro', ' 4 Quarto'];
+
+console.log(`MÉTODO .UNSHIFT()\nElementos: ${numeros}`);
+console.log(`Qnt de elementos: ${numeros.unshift(' 1 Primeiro')}`);
+console.log(`Com o .unshift: ${numeros}\n`);
+
+// JOIN()
+
+
+// SLICE()
+
+
+// CONCAT()
+
+
+// LENGTH()
+
+
+// INDEXOF()
+
+
+// LASTINDEXOF()
