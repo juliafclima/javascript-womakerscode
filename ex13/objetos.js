5d9f793e534323b1baf63b97d9e59746a1cf0717
juliafclima <juliafclima@hotmail.com>
2023-07-24 14:58:18 -0300

    Update objetos.js

--- ex13/objetos.js
+++ ex13/objetos.js
@@ -1,41 +1,44 @@
 // OBJETOS EM JAVASCRIPT
 
-let objeto = {}; // OBJETO VAZIO
-
-let pessoa = 'Júlia';
-
-objeto[pessoa] = 'Isso é uma pessoa'; 
-
-objeto[1+1+1] = 'Três'; // OU objeto[3] = 'Três';
-
-/*
-QUANDO USAR O BRACKET []
-
-console.log(objeto.3); -> ERRO DE SINTAXE! Com NÚMERICO precisa usar o [] (colchetes)
-*/
-
-console.log('0 ' + objeto[3] + '\n'); // = Três
-console.log('1 ' + objeto[1 + 1 + 1] + '\n'); // = Três
-
-console.log('2 ' + objeto[pessoa] + '\n'); // = 'Isso é uma pessoa';
-console.log('3 ' + objeto['Júlia'] + '\n'); // = 'Isso é uma pessoa';
-
-console.log('4 ' + objeto.pessoa + '\n'); // = undefined! Não consegue resgatar as própriedades do objeto
-
-/*
-QUANDO USAR O DOT . 
-*/
-const Pessoa = {
-    nome: 'Julia',
+let pessoa = {
+    nome: 'Júlia',
     sobrenome: 'Lima',
     idade: 21,
-    cor: 'Branca'
-};
+    cor: 'preta',
+    profissao: 'desenvolvedora'
+}
 
-//listarSobrenome = Pessoa.sobrenome;
-listarSobrenome = Pessoa['sobrenome']
+console.log(pessoa);
 
-console.log(Pessoa);
-console.log(listarSobrenome);
+// QUANDO USAR O DOT NOTATION . -> object.key
+// CHAVE ESTÁTICA -> Chave digitada diretamente
+// altera uma propriedade
+
+pessoa.idade = 22; 
+
+console.log(`\nTenho ${pessoa.idade} anos!`); // tenho 22 anos!
+
+// adiciona uma propriedade
+
+pessoa.nacionalidade = 'Brasileira'; 
+
+console.log(`\nSou ${pessoa.nacionalidade}!`); // sou Brasileira!
+
+// QUANDO USAR O BRACKET NOTATION [] -> object[expression]
+// CHAVE DINÂMICA -> chave validada a partir de uma expressão
+// altera uma propriedade
+
+pessoa['cor'] = 'branca'
+
+console.log(`\nMinha cor é ${pessoa['cor']}!`); // minha cor é branca!
+
+// adiciona uma propriedade 
+
+let nascida = pessoa['naturalidade'] = 'Petrópolis'; 
+
+console.log(`\nSou natural de ${nascida}!`); // sou natural de Petrópolis!
+
+
+
 
 
